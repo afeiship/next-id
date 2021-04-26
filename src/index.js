38b3f431854979a26341d1c6ab7d01e24664a8d7
afeiship <1290657123@qq.com>
2021-04-26 08:44:08 +0800

    style: format

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 (function () {
   var global = typeof window !== 'undefined' ? window : this || Function('return this')();
   var nx = global.nx || require('@jswork/next');
-var id = 0;
+  var id = 0;
 
   nx.id = function () {
     return ++id;
